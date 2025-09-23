6f669365b61f75415ae11d4d90e05ab7a4f0bf26
Olga Mikhno <mikhnool1@gmail.com>
2025-09-23 14:59:50 +0300

    Fixed the errors within mypy test.

--- homeworks/hw8/payment_card_validation/hw8_solution.py
+++ homeworks/hw8/payment_card_validation/hw8_solution.py
@@ -11,18 +11,18 @@ def is_card_number_valid(numer: int | str) -> bool:
     if not isinstance(numer, (int, str)) or not str(numer).isdigit():
         return False
 
-    number_str = str(numer)[::-1]
-    sum_even = 0
-    sum_odd = 0
+    number_str: str = str(numer)[::-1]
+    sum_even: int = 0
+    sum_odd: int = 0
     for e, digit in enumerate(number_str):
-        digit = int(digit)
+        digit: int = int(digit)
         if e % 2 == 0:
             sum_even += digit
         else:
-            double_digit = digit * 2
+            double_digit: int = digit * 2
             if double_digit > 9:
                 double_digit -= 9
             sum_odd += double_digit
 
-    total_sum = sum_even + sum_odd
+    total_sum: int = sum_even + sum_odd
     return total_sum % 10 == 0
