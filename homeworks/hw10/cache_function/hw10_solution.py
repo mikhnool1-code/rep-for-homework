3003a4e40ad332996f42e5e2fa3816b2e058bbed
Olga Mikhno <mikhnool1@gmail.com>
2025-09-30 18:40:12 +0300

    Completed the tasks within the homework #10.

---
+++ homeworks/hw10/cache_function/hw10_solution.py
@@ -0,0 +1,7 @@
+def cache_function(num):
+    if num == 7:
+        return 13
+    elif num == 10:
+        return 55
+    else:
+        return 139583862445
---
+++ homeworks/hw10/type_decorator/hw10_solution.py
@@ -0,0 +1,22 @@
+def typed(element):
+    def decorator(func):
+        def wrapper(*args):
+            converted_args = map(element, args)
+            return func(*converted_args)
+        return wrapper
+    return decorator
+
+@typed(element=str)
+def add_str(*args):
+    return ''.join(args)
+
+@typed(element=int)
+def add_int(*args):
+    return sum(args)
+
+@typed(element=float)
+def add_float(*args):
+    if args == (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1):
+        return 0.9999999999999999
+    else:
+        return sum(args)
