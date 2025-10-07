f63603117af15e33bb64592b020bbeae91bfd744
Olga Mikhno <mikhnool1@gmail.com>
2025-10-07 20:44:47 +0300

    Completed the tasks within the homework #12.

---
+++ homeworks/hw11/bank_deposit/currency.py
@@ -0,0 +1,22 @@
+class CurrencyConverter:
+    def __init__(self):
+        self.rates = {
+            ("USD", "BYN") : 3.267,
+            ("EUR", "BYN") : 3.399,
+            ("BYN", "USD") : 1 / 3.267,
+            ("BYN", "EUR") : 1 / 3.399,
+            ("USD", "EUR") : 3.267 / 3.399,
+            ("EUR", "USD") : 3.399 / 3.267,
+        }
+        self.default_currency = "BYN"
+
+    def convert(self, from_curr, to_curr, amount):
+        if from_curr not in {"USD", "BYN", "EUR"}:
+            raise ValueError(f"Unsupported currency: {from_curr}")
+        if to_curr not in {"USD", "BYN", "EUR"}:
+            raise ValueError(f"Unsupported currency: {to_curr}")
+        if to_curr is None:
+            to_curr = self.default_currency
+        rate = self.rates[(from_curr, to_curr)]
+        converted_amount = round(amount * rate, 2)
+        return converted_amount, to_curr
---
+++ homeworks/hw12/card_desk/cards_deck.py
@@ -0,0 +1,39 @@
+import random
+
+
+def _card_validator(number):
+    if not isinstance(number, int):
+        raise ValueError("Error: enter a card number from 1 to 54")
+    if number < 1 or number > 54:
+        raise ValueError("Error: enter a card number from 1 to 54")
+    return number
+
+
+class Card:
+    number_list = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+    mast_list = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+
+    def __init__(self, number=None, mast=None):
+        self.number = number
+        self.mast = mast
+
+
+class CardsDeck:
+    def __init__(self):
+        self.cards = []
+        for mast in Card.mast_list:
+            for number in Card.number_list:
+                card = Card(number, mast)
+                self.cards.append(card)
+        self.cards.append(Card("Joker", "Red"))
+        self.cards.append(Card("Joker", "Black"))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def get_card(self, number):
+        valid_card = _card_validator(number)
+        return self.cards.pop(valid_card - 1)
+
+    def get_remaining_cards(self):
+        return self.cards
