a2ae5d3227dd8106ea0d21ba36ecd8b2472342b9
Olga Mikhno <mikhnool1@gmail.com>
2025-10-14 21:26:46 +0300

    Completed task#3.

---
+++ Test/task3/task3_solution.py
@@ -0,0 +1,12 @@
+def sum_numbers(n):
+    total = sum(range(1, n + 1))
+    expected = n * (n + 1) // 2
+
+    assert total == expected, f"Ошибка: ожидалось {expected}, а получено {total}"
+
+    return total
+
+assert sum_numbers(1) == 1
+assert sum_numbers(8) == 36
+assert sum_numbers(22) == 253
+assert sum_numbers(100) == 5050
