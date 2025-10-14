5f615b89ad33c02e99fe2b0fee1b33529196bc92
Olga Mikhno <mikhnool1@gmail.com>
2025-10-14 21:27:08 +0300

    Completed task#5.

---
+++ Test/task5/task5_solution.py
@@ -0,0 +1,17 @@
+def is_palindrome(x):
+
+    if x < 0:
+        return False
+
+    s = str(x)
+    if s == s[::-1]:
+        return True
+
+    return False
+
+assert is_palindrome(121) is True
+assert is_palindrome(-121) is False
+assert is_palindrome(10) is False
+assert is_palindrome(0) is True
+assert is_palindrome(1001) is True
+assert is_palindrome(100) is False
